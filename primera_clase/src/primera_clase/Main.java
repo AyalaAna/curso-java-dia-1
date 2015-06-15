ddab178397893e663b34cb1f8257ad8f0a5509b9
laoratorio <laoratorio@atl-labobd-306>
2015-06-15 15:16:14 -0430

    e modifico el for

---
+++ primera_clase/src/primera_clase/Main.java
@@ -0,0 +1,23 @@
+
+package primera_clase;
+
+/**
+ *
+ * @author laoratorio
+ */
+public class Main {
+
+   private int contador=0;
+   
+    public static void main(String[] args) {
+
+        Main jp1=new  Main();
+     
+        
+        for (jp1.contador= 0; jp1.contador< 10; jp1.contador ++) {
+            System.out.println("imprimiendo el valor \n \n \n                                                                                                                                                                                                                                   " + jp1.contador);
+        }
+
+    }
+
+}
